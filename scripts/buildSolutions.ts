c7ce6917fc8f74a3d5d8da75a9081880b099f786
cf12 <11359669+cf12@users.noreply.github.com>
2022-04-16 05:45:54 -0400

    Add functional solution builder

--- scripts/buildSolutions.ts
+++ scripts/buildSolutions.ts
@@ -3,6 +3,8 @@ import fetch from "node-fetch"
 import { GraphQLClient, gql } from "graphql-request"
 import mysql from "mysql2/promise"
 import dotenv from "dotenv"
+import { Connection } from "mysql2"
+import seedrandom from 'seedrandom'
 
 type Anime = {
   id: number
@@ -12,30 +14,22 @@ type Anime = {
 }
 
 dotenv.config()
-
-function shuffle(array: any[]) {
-  let currentIndex = array.length,  randomIndex;
-
-  // While there remain elements to shuffle.
-  while (currentIndex != 0) {
-
-    // Pick a remaining element.
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    // And swap it with the current element.
-    [array[currentIndex], array[randomIndex]] = [
-      array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
+seedrandom(process.env.SEED || 'test_seed_do_not_use', {global: true})
 
 const client = new GraphQLClient("https://graphql.anilist.co/")
 
 const supabaseUrl = "https://fiztxwdqgnunsppccyti.supabase.co"
 const supabaseKey = process.env.SUPABASE_KEY || ""
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+const NUM_PAGES = 5
+
+function shuffle(array: any[]) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+}
 
 ;(async () => {
   const connection = await mysql.createConnection({
@@ -47,72 +41,76 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 
   let anime: Anime[] = []
 
-  const topAnilist = (
-    await client.request(gql`
+  for (let i = 1; i <= NUM_PAGES; i++) {
+    const res = await client.request(gql`
       {
-        Page(page: 1, perPage: 100) {
-          media(type: ANIME, sort: POPULARITY_DESC) {
+        Page(page: ${i}, perPage: 100) {
+          media(type: ANIME, sort: POPULARITY_DESC, isAdult: false) {
             id
             idMal
           }
         }
       }
     `)
-  ).Page.media
 
-  for (const entry of topAnilist) {
-    const { id, idMal }: { id: number; idMal: number } = entry
+    for (const entry of res.Page.media) {
+      const { id, idMal }: { id: number; idMal: number } = entry
 
-    let [rows, fields] = await connection.execute(
-      `
-    SELECT DISTINCT at2.slug, v.basename, v.size, v.resolution FROM resources r
-      JOIN anime_resource ar ON r.resource_id = ar.resource_id 
-      JOIN anime_themes at2 ON ar.anime_id = at2.anime_id 
-      JOIN anime_theme_entries ate ON at2.theme_id = ate.theme_id 
-      JOIN anime_theme_entry_video atev ON ate.entry_id = atev.entry_id 
-      JOIN videos v ON atev.video_id = v.video_id 
-    WHERE 
-      (r.external_id = ? AND r.site = 3) OR
-      (r.external_id = ? AND r.site = 7);
-    `,
-      [id, idMal]
-    )
+      let hash = new Set()
+      let [rows, fields] = await connection.execute(
+        `
+        SELECT DISTINCT at2.slug, v.basename, v.size, v.resolution FROM resources r
+          JOIN anime_resource ar ON r.resource_id = ar.resource_id 
+          JOIN anime_themes at2 ON ar.anime_id = at2.anime_id 
+          JOIN anime_theme_entries ate ON at2.theme_id = ate.theme_id 
+          JOIN anime_theme_entry_video atev ON ate.entry_id = atev.entry_id 
+          JOIN videos v ON atev.video_id = v.video_id 
+        WHERE 
+          (r.external_id = ? AND r.site = 3) OR
+          (r.external_id = ? AND r.site = 7);
+        `,
+        [id, idMal]
+      )
 
-    let hash = new Set()
+      Object.values(rows)
+        .filter(({ slug }) => slug.includes('OP'))
+        .sort((a, b) => {
+          return b.resolution - a.resolution || b.size - a.size
+        })
+        .forEach(({ slug, basename }: { basename: string; slug: string }) => {
+          if (!hash.has(slug)) {
+            // Update hash
+            hash.add(slug)
 
-    Object.values(rows)
-      .sort((a, b) => {
-        return b.resolution - a.resolution || b.size - a.size
-      })
-      .forEach(({ slug, basename }: { basename: string; slug: string }) => {
-        if (!hash.has(slug)) {
-          hash.add(slug)
-          anime.push({
-            id,
-            name: slug,
-            url: `https://animethemes.moe/video/${basename}`,
-          })
-        }
-      })
+            // Push to ret
+            anime.push({
+              id,
+              name: slug,
+              url: `https://animethemes.moe/video/${basename}`,
+            })
+          }
+        })
+    }
   }
 
+  // Shuffle by breakpoints
+  shuffle(anime)
+
+  // Populate dates
   let date = new Date()
-  anime = shuffle(anime).map((anime: Anime) => {
-    anime.date = date
+  anime = anime.map((entry) => {
+    entry.date = date
 
     const newDate = new Date(date)
     newDate.setDate(newDate.getDate() + 1)
     date = newDate
 
-    return anime
+    return entry
   })
 
-  console.log(anime)
-
-  const { data, error } = await supabase.from("solutions").upsert(anime)
-
-  if (error) console.error(error)
-  else console.log(data)
+  // const { data, error } = await supabase.from("solutions").upsert(anime)
+  // if (error) console.error(error)
+  // else console.log(data)
 
   await connection.end()
 })()
