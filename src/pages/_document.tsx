478cc3f2c907eaa2806a336f0c9552424a2815b5
cf12 <11359669+cf12@users.noreply.github.com>
2022-04-22 16:21:15 -0400

    Add metadata

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,9 +4,32 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <title>Kikudle - Wordle For Weebs!</title>
+        <meta property="og:title" content="Kikudle - Wordle For Weebs!" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kikudle.moe" />
+        <meta
+          property="og:image"
+          content="https://kikudle.moe/images/thumbnail.png"
+        />
+        <meta
+          property="og:description"
+          content='Kikudle is the wordle game for weebs! Guess the daily anime based on bits and pieces of the opening theme. After each attempt, you&apos;ll receive even more hints...
+          '
+        />
+        <meta name="theme-color" content="#FFFFFF" />
+        <meta name="twitter:card" content="summary_large_image" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Noto+Sans+JP:wght@400;500;700&display=swap" rel="stylesheet" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;400;700&family=Noto+Sans+JP:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
